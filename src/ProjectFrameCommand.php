27076fe35dc4ad8183f38194e8f5c6fdf271ae9b
Thura <waithura13579@gmail.com>
2023-02-15 21:42:27 +0630

    Update commit

--- src/ProjectFrameCommand.php
+++ src/ProjectFrameCommand.php
@@ -6,16 +6,19 @@ use Illuminate\Console\Command;
 
 class ProjectFrameCommand extends Command
 {
-    const C_DAO = "app/Contracts/Dao";
-    const SERVICE = "app/Services";
-    const C_SERVICE = "app/Contracts/Services";
-    const DAO = "app/Dao";
+    private $dirArray = [
+        "app/Contracts/Dao",
+        "app/Services",
+        "app/Contracts/Services",
+        "app/Dao",
+    ];
+    
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'make:frame {name*}';
+    protected $signature = 'create:frame {name*}';
 
     /**
      * The console command description.
@@ -57,9 +60,11 @@ class ProjectFrameCommand extends Command
     }
 
     public function makeDir() {
-        mkdir(self::C_DAO);
-        mkdir(self::C_SERVICE);
-        mkdir(self::SERVICE);
-        mkdir(self::DAO);
+        foreach($this->dirArray as $value) {
+            if(!is_dir($value)) {
+                mkdir($value);
+                chmod($value,0777);
+            }
+        }
     }
 }
